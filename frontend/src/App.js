2a94ad1cc25513b7af88ec68c06930936aebbb85
singhabhishek9835 <32333291+singhabhishek9835@users.noreply.github.com>
2023-03-28 20:47:52 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [userName, setUsername] = useState('')
+
+  useEffect(() => {
+    getNames();
+  }, [])
+
+  const getNames = async () => {
+    const response = await axios.get('/names');
+    console.log(response);
+    setUsername(response.data)
+  }
+
+  return (
+    <>
+      <h1>My Website</h1>
+      <h3>My name is {userName}</h3>
+    </>
+  );
+}
+
+export default App;
