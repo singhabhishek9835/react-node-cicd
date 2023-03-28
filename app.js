ebdf70691bf7092e79d4d15b2bbb31e80f4a83bb
singhabhishek9835 <32333291+singhabhishek9835@users.noreply.github.com>
2023-03-28 20:52:48 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+app.get('/names', (req, res) => {
+  res.send("Telmo Sampaio");
+})
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+})
